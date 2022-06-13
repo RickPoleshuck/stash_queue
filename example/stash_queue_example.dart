d90410ef380d3343c1b95e35efa7fd4cb494b291
Rick Poleshuck <RickPoleshuck@gmail.com>
2022-06-13 16:26:33 -0500

    allow compatibility with integration_test

--- example/stash_queue_example.dart
+++ example/stash_queue_example.dart
@@ -1,7 +1,7 @@
 import 'package:stash_queue/stash_queue.dart';
 
 void main() async {
-  var fifo = await FifoQueue.create('/directory', 'vaultName');
+  var fifo = await FifoQueue.create('/tmp/fifo', 'vaultName');
   int size = await fifo.size;
   print('fifo: $size');
 }
--- lib/src/stash_queue_base.dart
+++ lib/src/stash_queue_base.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:stash/stash_api.dart';
 import 'package:stash_file/stash_file.dart';
@@ -10,10 +11,12 @@ class FifoQueue {
   FifoQueue._create();
 
   late Vault<String> _vault;
+  late RandomAccessFile _fileLock;
 
   Future<void> _init(String storePath, String name) async {
     final store = await newFileLocalVaultStore(path: storePath);
     _vault = await store.vault<String>(name: name);
+    _fileLock = File('$storePath/$name/lock').openSync(mode: FileMode.write);
   }
 
   static Future<FifoQueue> create(String storePath, String vaultName) async {
@@ -23,38 +26,66 @@ class FifoQueue {
   }
 
   Future<int> get size async {
-    var ptrRecord = await _getPtrRecord();
-    return ptrRecord.size;
+    try {
+      _fileLock.lockSync(FileLock.exclusive);
+      var ptrRecord = await _getPtrRecord();
+      return ptrRecord.size;
+    } finally {
+      _fileLock.unlockSync();
+    }
   }
 
   // potentially slow as all files are removed
   Future<void> erase() async {
-    while(await size != 0) {
-      await get();
+    try {
+      _fileLock.lockSync(FileLock.exclusive);
+      while (await size != 0) {
+        await get();
+      }
+    } finally {
+      _fileLock.unlockSync();
     }
   }
 
   // clear is fast but leaves orphan files
   Future<void> clear() async {
-    await _putPtrRecord(PtrRecord(0, 0));
+    try {
+      _fileLock.lockSync(FileLock.blockingExclusive);
+      PtrRecord ptrRecord = await _getPtrRecord();
+      if (ptrRecord.size > 0) {
+        await _putPtrRecord(PtrRecord(0, 0));
+      }
+    } finally {
+      _fileLock.unlockSync();
+    }
   }
 
   Future<void> put(String value) async {
-    var ptrRecord = await _getPtrRecord();
-    await _vault.put(ptrRecord.end.toString(), value);
-    ptrRecord.end++;
-    await _putPtrRecord(ptrRecord);
+    try {
+      _fileLock.lockSync(FileLock.blockingExclusive);
+      var ptrRecord = await _getPtrRecord();
+      await _vault.put(ptrRecord.end.toString(), value);
+      ptrRecord.end++;
+      await _putPtrRecord(ptrRecord);
+    } finally {
+      _fileLock.unlockSync();
+    }
   }
 
   Future<String> get() async {
-    var ptrRecord = await _getPtrRecord();
-    if (ptrRecord.size <= 0) {
-      throw RangeError('FIFO queue is empty');
+    try {
+      _fileLock.lockSync(FileLock.exclusive);
+      var ptrRecord = await _getPtrRecord();
+      if (ptrRecord.size <= 0) {
+        throw RangeError('FIFO queue is empty');
+      }
+      String result = await _vault.getAndRemove(ptrRecord.start.toString()) ?? '';
+      ptrRecord.start++;
+      await _putPtrRecord(ptrRecord);
+      return result;
+    } finally {
+      _fileLock.unlockSync();
     }
-    String result = await _vault.getAndRemove(ptrRecord.start.toString()) ?? '';
-    ptrRecord.start++;
-    await _putPtrRecord(ptrRecord);
-    return result;
   }
 
   Future<void> _putPtrRecord(PtrRecord ptrRecord) async {
@@ -63,21 +94,23 @@ class FifoQueue {
   }
 
   Future<PtrRecord> _getPtrRecord() async {
-    var ptrRecord = await _vault.get(pointerKey);
-    ptrRecord ??= '{"s": 0, "e": 0}';
-    PtrRecord result = PtrRecord.fromJson(jsonDecode(ptrRecord));
-    if (result.size == 0) {
+    var ptrStr = await _vault.get(pointerKey);
+    ptrStr ??= '{"s": 0, "e": 0}';
+    PtrRecord ptrRecord = PtrRecord.fromJson(jsonDecode(ptrStr));
+    if (ptrRecord.size == 0) {
       // don't keep increasing pointers unnecessarily
-      result.start = result.end = 0;
+      ptrRecord.start = ptrRecord.end = 0;
     }
-    return result;
+    return ptrRecord;
   }
 }
 
 class PtrRecord {
   int start;
   int end;
+
   get size => end - start;
+
   PtrRecord(this.start, this.end);
 
   PtrRecord.fromJson(Map<String, dynamic> json)
